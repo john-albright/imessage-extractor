47faf50209f7c985f9ae803c67f3bbd335ca4dae
John Albright <John@john-albrights-macbook-pro.local>
2023-06-20 18:52:43 -0500

    First successful draft: added environment variables for privacy, allows user to choose to download images or record the paths of the images in a .txt file, added a pathway that allows the program to use .abcddb files to get contact information (in place of using a .vcf file), created global variables to act as settings for the new options; commented code is an attempt to create subfolders and add set the first dictionary of each group chat in the textHistory dictionary

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # iMessage Extractor
+
+This iMessage Extractor was created as the final project for CS50x Introduction to Computer Science. It builds on Python and SQL skills taught in Weeks 7 and8 of the course. This application is a command-line tool that can be run with the command `python imessage_extractor.py`. It works in tandem with `sqlite3` and `dotenv` to extract every conversation stored on a Macbook Pro.
+
+To use the file, a .env file must be created containing the paths of the database used for the Apple Contacts application. These environment variables are to be named `CONTACT_DB` and `CHAT_DB`. Here's a sample of what that .env file may look like:
+```
+export CONTACT_DB="/Users/userName/Library/Application Support/AddressBook/Sources/872138947528352384907502735/AddressBook-v22.abcddb"
+export CHAT_DB="/Users/userName/Library/Messages/chat.db"
+```
+
+The program can also be run if a `contacts.vcf` file is saved in the same folder as the principal python file. This can be created by opening the Apple Contacts application, selecting all the contacts (ctl + A), and navigating to File > Export > Export vCard... The .abcddb file may not be completely up to date on your MacBook, so exporting a vCard file may be a better strategy.
+
+By default, the program will process the `contacts.vcf` file before using the `CHAT_DB` flag stored as an environment variable. This can be changed by switching the flag `USE_VCF` found at the top of the `imessage_extractor.py` file to `True`. 
+
+If the user is concerned about the image files taking up space, there is the option to create a .txt file called `images.txt` that will store all the filepaths (somewhere in your MacBook Pro's Library folder) and the date/times they were sent and the source. If the `SAVE_IMAGE_FILES` flag at the top of the `imessage_extractor.py` file is `True`, the images will save to an `images` folder within the folder of assigned to each person or group.  
+
+Various websites and discussions were referred to for this project: [a Medium article showing how the chat database is structured for MacBooks](https://medium.com/@yaskalidis/heres-how-you-can-access-your-entire-imessage-history-on-your-mac-f8878276c6e9), [a StackOverflow discussion post showing how to use regular expressions to parse vCard files](https://stackoverflow.com/questions/54551712/reading-vcf-file-data-using-python-and-re-library), and a [StackOverflow discussion post showing how to strip non-ASCII characters from a string](https://stackoverflow.com/questions/123336/how-can-you-strip-non-ascii-characters-from-a-string-in-c).  
--- imessage_extractor.py
+++ imessage_extractor.py
@@ -5,19 +5,33 @@ import sys
 import glob
 import os
 import shutil
+import platform
 from datetime import datetime
+from dotenv import load_dotenv
 
-# vcf help: https://stackoverflow.com/questions/54551712/reading-vcf-file-data-using-python-and-re-library
-# iMessage db help: https://medium.com/@yaskalidis/heres-how-you-can-access-your-entire-imessage-history-on-your-mac-f8878276c6e9
-# strip non-ASCII characters: https://stackoverflow.com/questions/123336/how-can-you-strip-non-ascii-characters-from-a-string-in-c
+# Flag to determine whether image files are saved or a .txt file with their paths is created
+SAVE_IMAGE_FILES = False
 
+# Flag to determine whether the vcf file should be processed 
+USE_VCF = True
 
-def convertEpochBfHighSierra(time):
-	return 
+# Load environment variables from .env file
+load_dotenv()
+chat_db_location = os.getenv('CHAT_DB')
+apple_contact_db_location = os.getenv('CONTACT_DB')
+
+# Get system information about the current MacBook
+mac_info = platform.mac_ver()
+os_version = mac_info[0]
+os_sub_version = int(re.findall('(?<=[.]).*(?=[.])', os_version)[0])
+high_sierra_min = True if os_sub_version > 11 else False
 
 if __name__ == '__main__':
+	# Start the timer
+	start_time = datetime.now()
+
 	# Connect to the database
-	db = sqlite3.connect('/Users/John/Library/Messages/chat.db')
+	db = sqlite3.connect(chat_db_location)
 
 	# Create a cursor
 	cur = db.cursor()
@@ -38,36 +52,86 @@ if __name__ == '__main__':
 	# Example key-value pair: { 'poodle@gmail123.com': 'Favorite Poodle' }
 	contactDict = {}
 
-	# Open vCard file 
-	vcfFile = open('contacts.vcf', 'r')
-
+	# Initialize strings to be emptied and refilled if using a vcf file
+	# Otherwise initialize strings to be filled when searching the Apple .db file
 	name = ''
 	email = ''
 	telephone = ''
-	nameReg = 'FN:(.*)'
-	emailReg = 'EMAIL.*:(.*)'
-	telReg = 'TEL.*:(.*)'
 
-	for line in vcfFile:
-		if re.search(nameReg, line):
-			name = re.findall(nameReg, line)[0]
-		if re.search(emailReg, line):
-			email = re.findall(emailReg, line)[0]
-		if re.search(telReg, line):
-			telephone = re.findall(telReg, line, re.MULTILINE)[0]
-			telephone = re.sub('[^\u0000-\u007F]+', '', telephone)
-			telephone = re.sub('[^\d]*', '', telephone)
-		if re.search('END:VCARD.*', line):
+	# Declare regex strings to be used for filtering phone numbers 
+	removeUnicodeReg = '[^\u0000-\u007F]+'
+	removeNondigitsReg = '[^\d]*'
+
+	if USE_VCF:
+		# Open vCard file 
+		try:
+			vcfFile = open('contacts.vcf', 'r')
+		except FileNotFoundError:
+			print('No file \'contacts.vcf\' found in the parent directory.')
+			sys.exit(0)
+
+		nameReg = 'FN:(.*)'
+		emailReg = 'EMAIL.*:(.*)'
+		telReg = 'TEL.*:(.*)'
+
+		for line in vcfFile:
+			if re.search(nameReg, line):
+				name = re.findall(nameReg, line)[0]
+			if re.search(emailReg, line):
+				email = re.findall(emailReg, line)[0]
+			if re.search(telReg, line):
+				telephone = re.findall(telReg, line, re.MULTILINE)[0]
+				telephone = re.sub(removeUnicodeReg, '', telephone)
+				telephone = re.sub(removeNondigitsReg, '', telephone)
+			if re.search('END:VCARD.*', line):
+				name = ''
+			
+			if name:
+				if email:
+					contactDict[email] = name
+					email = ''
+				if telephone:
+					contactDict[telephone] = name
+					telephone = ''
+	else:
+		#print(apple_contact_db_location)
+		if os.path.isfile(apple_contact_db_location):
+			db2 = sqlite3.connect(apple_contact_db_location)
+		else:
+			print(f'No file path ${apple_contact_db_location} exists.')
+			sys.exit(1)
+
+		cur2 = db2.cursor()
+
+		# Perform an SQL operation to select all phone numbers and emails and the first and 
+		# last names associated with those by joining the tables ZABCDRECORD, ZABCDPHONENUMBER,
+		# and ZABCDEMAILADDRESS
+		cur2.execute('SELECT ph.ZFULLNUMBER, em.ZADDRESSNORMALIZED, ZFIRSTNAME, ZLASTNAME FROM ZABCDRECORD INNER JOIN ZABCDPHONENUMBER AS ph ON ZABCDRECORD.Z_PK = ph.ZOWNER INNER JOIN ZABCDEMAILADDRESS AS em ON ZABCDRECORD.Z_PK = em.ZOWNER')
+
+		for contact in cur2.fetchall():
+			#print(contact)
+			#continue
+			telephone = contact[0]
+			telephone = re.sub(removeUnicodeReg, '', telephone)
+			telephone = re.sub(removeNondigitsReg, '', telephone)
+			email = contact[1]
+			if contact[2]:
+				name = contact[2]
+			if contact[3]:
+				name = name + " " + contact[3] 
+			if not name:
+				print(telephone, "No name")
+				continue
+			
+			# Map telephone numbers and email addresses to full names
+			contactDict[telephone] = name
+			contactDict[email] = name
+
+			# Reset all variables
 			name = ''
-		
-		if name:
-			if email:
-				contactDict[email] = name
-				email = ''
-			if telephone:
-				contactDict[telephone] = name
-				telephone = ''
-		
+			telephone = ''
+			email = ''
+
 	#print(contactDict)
 
 	# Initialize the dictionary that will map cache roomnames to group names
@@ -88,11 +152,13 @@ if __name__ == '__main__':
 
 	#print(groupsDict)
 
+	#sys.exit(4)
+
 	# Initialize the dictionary that will map chat ids to image file paths
 	# Example key-value pair: { '621': { 'filepath': '~\Library\IMG_6542.jpg', 'name': 'IMG_652.jpg' }}
 	attachments = {}
 
-	cur.execute('SELECT message_attachment_join.message_id, filename, transfer_name FROM attachment INNER JOIN message_attachment_join on attachment.ROWID=message_attachment_join.attachment_id')
+	cur.execute('SELECT message_attachment_join.message_id, filename, transfer_name FROM attachment INNER JOIN message_attachment_join ON attachment.ROWID=message_attachment_join.attachment_id')
 
 	for attachment in cur.fetchall():
 		#filepath = re.sub('^~', '/Users/John', attachment[1])
@@ -104,11 +170,15 @@ if __name__ == '__main__':
 	#sys.exit(0)
 
 	# Get information from the messages table
-	cur.execute('SELECT handle_id, date, text, is_from_me, account, ROWID, cache_roomnames, cache_has_attachments, service FROM message ORDER BY ROWID DESC LIMIT 1000')
+	cur.execute('SELECT handle_id, date, text, is_from_me, account, ROWID, cache_roomnames, cache_has_attachments, service FROM message ORDER BY ROWID DESC')
 
 	# Initialize the list that will map the name to the message
 	# Example key-value pair: { 'Johnny Appleseed': [{ 'source': 'email', 'time': 16088449385, 
 	# 'text': 'Hello'}, { 'source': 188888888, 'time': 16088449387, 'text': 'Hello' }] }
+	# Example key-value pair for group chats: { 'group3': [{ 'memberNumbers': ['11111111', '222222',
+	# '333333'], { 'source': '1111111', 'time': 187293742837, 'text': 'No way!' } }]}
+	#
+
 	textHistory = {}
 	miscContactNum = 0
 
@@ -120,7 +190,13 @@ if __name__ == '__main__':
 	for message in cur.fetchall():
 		handleId = message[0]
 		#time = datetime(str(message[1]) + datetime.strftime("%s", "2001-01-01") , "unixepoch", "localtime")
-		time = datetime.utcfromtimestamp(duration.total_seconds() + message[1]).strftime('%Y-%m-%d %H:%M:%S')
+		seconds = message[1]
+		
+		# Divide seconds by a million if the current OSx is High Sierra or higher
+		if high_sierra_min:
+			seconds = seconds / 10 ** 9
+		
+		time = datetime.utcfromtimestamp(duration.total_seconds() + seconds).strftime('%Y-%m-%d %H:%M:%S')
 		text = message[2]
 		isFromMe = True if message[3] == 1 else False
 		myAccount = message[4]
@@ -164,42 +240,126 @@ if __name__ == '__main__':
 				infoObj['photofilepath'] = attachments[rowId]['filepath']
 				infoObj['text'] = infoObj['text'] + attachments[rowId]['name']
 				#print(infoObj)
-			if not textHistory.get(name):
-				textHistory[name] = []
+			#if not textHistory.get(name):
+			#	textHistory[name] = []
+				#if groupNum: 
+				#	contactsObj = { "memberNumbers" : [] }
+				#	textHistory[name].append(contactsObj)
 			if groupNum:
+				# Update the first object with the contact information
+				#firstObj = textHistory[name][0].get("memberNumbers")
+				#print(message, textHistory, source, firstObj)
+				#if firstObj != None:
+				#	if source not in firstObj and not isFromMe:
+				#		textHistory[name][0]["memberNumbers"].append(source)
+				
 				memberName = contactDict.get(source)
 				if not memberName:
 					miscContactNum += 1
 					contactDict[source] = f'contact{miscContactNum}'
 				infoObj['memberName'] = 'me' if isFromMe else contactDict[source]
+				#textHistory[name].insert(1, infoObj)
+			#else:
+			if not textHistory.get(name):
+				textHistory[name] = []
 			
 			textHistory[name].insert(0, infoObj)
 
 	# Get current working directory
 	cwd = os.getcwd()
+	count = 0
 
 	for key in textHistory:
 		#print(key, textHistory[key], '\n')
+		#count += 1
+		#if count > 10:
+		#	sys.exit(3)
+		#else:
+		#	continue
 		name = key.replace(" ", "_")
 
-		# Create folder 
+		# Create folder  
 		dstDir = cwd + "/messages/" + name + "/"
 		os.makedirs(dstDir, exist_ok=True)
+
+		# Create sub-folders
+		#if textHistory[key][0].get("memberNumbers"):
+		#if groupNum:
+		#	dstDir = dstDir + "groups/"
+		#	os.makedirs(dstDir, exist_ok=True)
+
+			#all_numbers_stored = True
+
+			#for group_member_number in textHistory[key][0].get("memberNumbers"):
+			#	if not contactDict.get(group_member_number):
+			#		all_numbers_stored = False
+			#		break
+			#if not all_numbers_stored:
+			#	dstDir = dstDir + "not_saved/" + name + "/"
+			#else:
+			#	dstDir = dstDir + "saved/" + name + "/"
+		#	dstDir = dstDir + name + "/"
+		#else: 
+		#	dstDir = dstDir + "individuals/"
+		#	os.makedirs(dstDir, exist_ok=True)
+
+		#	if 'contact' in key:
+		#		dstDir = dstDir + "not_saved/" + name + "/"
+		#	else:
+		#		dstDir = dstDir + "saved/" + name + "/"
+
+		#os.makedirs(dstDir, exist_ok=True)
 
 		# Create path for .txt file and start writing the file 
 		textfile = dstDir + name + ".txt"
 		f = open(textfile, 'w')
 		#print(f'{textDict["source"]} @ {textDict["time"]}]:{textDict["text"]}\n')
 
+		imgDir = dstDir + "images/"
+				
+		if textHistory.get("memberName"):
+			f.write(f'GROUP CHAT\n')
+
+		#is_group = hasattr(textHistory[key][0], "memberNumbers")
+		#print(is_group)
+
 		for textDict in textHistory[key]:
 			# Get image information
 			if textDict.get('photofilepath'):
-				imgFile = os.path.join(os.path.expanduser(textDict['photofilepath']))
-				#print(imgFile)
-				shutil.copy(imgFile, dstDir)
+				if SAVE_IMAGE_FILES:
+					os.makedirs(imgDir, exist_ok=True)
+					imgFile = os.path.join(os.path.expanduser(textDict['photofilepath']))
+					#print(imgFile)
+					shutil.copy(imgFile, dstDir)
+				else:
+					img_file = dstDir + name + "_images" + ".txt"
+					f2 = open(img_file, 'w+')
+					f2.write(f'[{textDict["source"]} @ {textDict["time"]}]: {textDict["photofilepath"]}\n')
+					
+			
 			#print(textDict)
 
-			if textDict
+			#if is_group:
+			#	if textDict.get("memberNumbers"):	
+			#		group_members_string = 'Group members:\n'
+			#		if textDict["memberNumbers"] != None:
+			#			for number in textDict["memberNumbers"]:
+			#				group_members_string += f"{number} ({contactDict[number]})\n"
 
-			f.write(f'[{textDict["source"]} @ {textDict["time"]}]: {textDict["text"]}\n')
+			#		f.write(group_members_string + "\n")
+			#	else:
+			#		f.write(f'[{textDict["source"]} ({textDict["memberName"]}) @ {textDict["time"]}]: {textDict["text"]}\n')
+			#else:
+			#	try: 
+					f.write(f'[{textDict["source"]} @ {textDict["time"]}]: {textDict["text"]}\n')
+			#	except KeyError:
+			#		print(textHistory[key][1], textDict)
+
+
 			
+	end_time = datetime.now()
+	time_diff = end_time - start_time
+	program_run_seconds = time_diff.total_seconds()
+	program_run_milliseconds = program_run_seconds / 60
+
+	print(f"Program run time: {program_run_seconds} seconds and {program_run_milliseconds} milliseconds")
